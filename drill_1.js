6c00940cccf7ce8205c0c349953936412ff79b2c
Arpan Patel <adp8ke@gmail.com>
2019-06-03 14:00:35 -0400

    added return to Drills_1

--- drill_1.js
+++ drill_1.js
@@ -1,6 +1,6 @@
 function createGreeting(){
-    console.log("Hi, my name is Arpan and I'm 23 years old");
+    return `Hi, my name is Arpan and I'm 23 years old`;
 }
 
-createGreeting();
-
+const greeting1 = createGreeting();
+console.log(greeting1);
