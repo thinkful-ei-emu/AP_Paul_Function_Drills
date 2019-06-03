ceb720f56bcfcab81727c6221977bc02e506c65b
Arpan Patel <adp8ke@gmail.com>
2019-06-03 14:01:53 -0400

    added parameters to drills_1

--- drill_1.js
+++ drill_1.js
@@ -1,6 +1,4 @@
-function createGreeting(){
+function createGreeting(name,age){
     return `Hi, my name is Arpan and I'm 23 years old`;
 }
 
-const greeting1 = createGreeting();
-console.log(greeting1);
