7aa179292b48825d9c7ad5f38a95bad871356000
Paul Han <kyutae.han93@gmail.com>
2019-06-03 14:08:45 -0400

    Added default for test.

--- drill_1.js
+++ drill_1.js
@@ -1,4 +1,7 @@
+'use strict';
 function createGreeting(name,age){
-    return `Hi, my name is Arpan and I'm 23 years old`;
+  return `Hi, my name is Arpan and I'm 23 years old`;
 }
+
+console.log('hello');
 
